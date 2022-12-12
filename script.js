25d8a8090b1d51da30fb42086a56cd96f88116f1
kaerukuujo <mrjolyne@gmail.com>
2022-12-12 15:59:18 +0000

    containers in html added, JScript to be refactored

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let myLibrary = [];
+const library = document.getElementsByClassName(libraryDisplay)[0];
+let brtn = document.getElementsByClassName('addBook')[0];
+brtn.addEventListener("click", clicked);
+
+let title = null;
+let author = null;
+let pages = null;
+let read = false;
+
+function clicked(){
+    title = document.getElementById(title).value;
+    author = document.getElementById(author).value;
+    pages = document.getElementById(pages).value;
+    read = document.getElementById(read).value;
+
+    addBookToLibrary(Book(title, author, pages, read));
+}
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -7,6 +24,14 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-function addBookToLibrary(){
+function addBookToLibrary(book){
     
+    let book = document.createElement(div.book);
+    const para = document.createElement("p");
+    const bookTitle = book.title;    
+    const bookAuthor = book.author;
+    const bookPages = book.pages;
+    const bookRead = book.read;
+    para.appendChild(bookTitle, bookAuthor, bookPages, bookRead);
+    library.appendChild(book);
 }
